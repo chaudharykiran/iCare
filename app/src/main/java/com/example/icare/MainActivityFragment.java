7aae25fc22c28cc7be4e641ea012a2e8cde21200
Kiran Kumar Chaudhary <hellokiran2020@gmail.com>
2016-01-06 00:33:38 +0545

    Simple UI for ItemListActivity created.

--- app/src/main/java/com/example/icare/MainActivityFragment.java
+++ app/src/main/java/com/example/icare/MainActivityFragment.java
@@ -1,15 +1,18 @@
 package com.example.icare;
 
+import android.content.Intent;
 import android.support.v4.app.Fragment;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 /**
  * A placeholder fragment containing a simple view.
  */
-public class MainActivityFragment extends Fragment {
+public class MainActivityFragment extends Fragment
+        implements View.OnClickListener {
 
     public MainActivityFragment() {
     }
@@ -17,6 +20,19 @@ public class MainActivityFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_main, container, false);
+        View rootView =  inflater.inflate(R.layout.fragment_main, container, false);
+
+        // Handler when More text is clicked
+        TextView textView = (TextView) rootView.findViewById(R.id.favorites);
+        textView.setOnClickListener(this);
+
+        return rootView;
+    }
+
+    @Override
+    public void onClick(View view) {
+        Intent intent = new Intent(getActivity(), ItemListActivity.class);
+
+        getActivity().startActivity(intent);
     }
 }
