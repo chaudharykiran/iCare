ae3fba934147b34c1f72978100491420452bf9ce
Kiran Kumar Chaudhary <hellokiran2020@gmail.com>
2016-01-09 02:30:16 +0545

    RecyclerView Adapter added which also handles click event on its items.

--- app/src/main/java/com/example/icare/ItemListActivityFragment.java
+++ app/src/main/java/com/example/icare/ItemListActivityFragment.java
@@ -1,11 +1,15 @@
 package com.example.icare;
 
+import android.content.Context;
 import android.support.v4.app.Fragment;
 import android.os.Bundle;
 import android.support.v7.widget.CardView;
+import android.support.v7.widget.DefaultItemAnimator;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.util.Log;
 import android.view.LayoutInflater;
+import android.view.MotionEvent;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
@@ -15,6 +19,8 @@ import android.widget.TextView;
 import com.gc.materialdesign.views.Card;
 
 import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Collections;
 import java.util.List;
 
 /**
@@ -22,11 +28,13 @@ import java.util.List;
  */
 public class ItemListActivityFragment extends Fragment {
 
+    private final static String LOG_TAG = "ItemListActivityFragment";
+
     /* private instance variable. */
-//    private List<Item> items;
+    private List<Item> items;
 
     /* private instance variable for adapter */
-//    private ItemAdapter itemAdapter;
+    private ItemAdapter itemAdapter;
 
 
     public ItemListActivityFragment() {
@@ -45,17 +53,18 @@ public class ItemListActivityFragment extends Fragment {
         // use LinearLayoutManager
         LinearLayoutManager linearLayoutManager = new LinearLayoutManager(getActivity());
         categoryRecyclerView.setLayoutManager(linearLayoutManager);
-//
-//        // Create some data
-//        initializeDate();
-//
-//        itemAdapter = new ItemAdapter(items);
-//        cardView.setAdapter(itemAdapter);
+
+
+        // Create some data
+        initializeDate();
+
+        itemAdapter = new ItemAdapter(getActivity(), items);
+        categoryRecyclerView.setAdapter(itemAdapter);
 
         return rootView;
     }
 
-/*    private class Item {
+    private class Item {
         private String itemName;
         private int itemPhotoId;
 
@@ -70,47 +79,67 @@ public class ItemListActivityFragment extends Fragment {
         items.add(new Item("Item 1", R.drawable.item1));
         items.add(new Item("Item 2", R.drawable.item2));
         items.add(new Item("Item 3", R.drawable.item4));
-    }*/
-
-
-
-/*    public static class ViewHolder extends RecyclerView.ViewHolder {
-        CardView cv;
-        TextView itemName;
-        ImageView itemPhotoId;
-
-        ViewHolder(View itemView) {
-            super(itemView);
-
-            cv = (CardView) itemView.findViewById(R.id.item_card_view);
-            itemName = (TextView) itemView.findViewById(R.id.item_name);
-            itemPhotoId = (ImageView) itemView.findViewById(R.id.item_photo);
-        }
-    }*/
+    }
 
     // Creating an Adapter
     private class ItemAdapter extends RecyclerView.Adapter<ItemAdapter.ItemViewHolder> {
 
+        /* private instance variable to store Layout of each item. */
+        private LayoutInflater inflater;
+        /* Store data */
+        List<Item> items = Collections.emptyList();
+
+        // Constructor to inflate layout of each item in RecyclerView
+        public ItemAdapter(Context context, List<Item> items) {
+            inflater = LayoutInflater.from(context);
+            this.items = items;
+        }
+
         @Override
         public ItemViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
-            return null;
+            Log.v(LOG_TAG, "onCreateViewHolder called.");
+            View view = inflater.inflate(R.layout.category_item, parent,false);
+
+            ItemViewHolder holder = new ItemViewHolder(view);
+
+            return holder;
         }
 
         @Override
         public void onBindViewHolder(ItemViewHolder holder, int position) {
+            Log.v(LOG_TAG, "onBindViewHolder called.");
+            Item current = items.get(position);
 
+            holder.itemName.setText(current.itemName);
+            holder.itemPhoto.setImageResource(current.itemPhotoId);
+
+            // click event handler when Item in RecyclerView is clicked
+            holder.cardView.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    Log.v(LOG_TAG, "Item Clicked.");
+                }
+            });
         }
 
         @Override
         public int getItemCount() {
-            return 0;
+            return items.size();
         }
 
         /* ViewHolder for this adapter */
         class ItemViewHolder extends RecyclerView.ViewHolder {
 
+            TextView itemName;
+            ImageView itemPhoto;
+            android.support.v7.widget.CardView cardView;
+
             public ItemViewHolder(View itemView) {
                 super(itemView);
+
+                itemName = (TextView) itemView.findViewById(R.id.item_name);
+                itemPhoto = (ImageView) itemView.findViewById(R.id.item_photo);
+                cardView = (android.support.v7.widget.CardView) itemView.findViewById(R.id.item_card_view);
             }
         }
     }
