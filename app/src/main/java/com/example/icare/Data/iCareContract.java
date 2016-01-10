480b6c5abf5ae727c6bce44fcdee99bd1fe2aaa8
Kiran Kumar Chaudhary <hellokiran2020@gmail.com>
2016-01-10 21:08:58 +0545

    iCareContract added and content authority and possible paths added.

--- app/src/main/java/com/example/icare/Data/iCareContract.java
+++ app/src/main/java/com/example/icare/Data/iCareContract.java
@@ -7,5 +7,17 @@ package com.example.icare.Data;
  * our Content Provider
  */
 public class iCareContract {
+    // The "Content Authority" is the name of entire content provider similar
+    // to the relationship between our domain name and its site.A convenient string to use for the
+    // content authority is the package name for the app, which is guaranteed to be unique on the
+    // device.
+    public static final String CONTENT_AUTHORITY = "com.example.icare.provider";
 
+    // Possible paths
+    // For instance : content:://com.example.icare.provider/food/ is valid at
+    // accessing weather data. content::/com.example.icare.provider/givemefood/ will
+    // fail.
+    public static final String PATH_FOOD = "food";
+    public static final String PATH_EXERCISE = "food";
+    public static final String PATH_BODY = "body";
 }
