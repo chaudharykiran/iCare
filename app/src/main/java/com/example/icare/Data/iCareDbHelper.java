befe752b5132f750d71f88b479a8da238ad527be
Kiran Kumar Chaudhary <hellokiran2020@gmail.com>
2016-01-10 22:55:32 +0545

    iCareDbHelper initial version completed.

--- app/src/main/java/com/example/icare/Data/iCareDbHelper.java
+++ app/src/main/java/com/example/icare/Data/iCareDbHelper.java
@@ -16,6 +16,7 @@ public class iCareDbHelper extends SQLiteOpenHelper {
     // Database version
     private static final int DATABASE_VERSION = 1;
 
+    // iCareDbHelper to create database
     public iCareDbHelper(Context context) {
         super(context, DATABASE_NAME, null, DATABASE_VERSION);
     }
@@ -23,10 +24,41 @@ public class iCareDbHelper extends SQLiteOpenHelper {
 
     @Override
     public void onCreate(SQLiteDatabase db) {
+
+        // SQL statement in order to create Food table
+        final String SQL_CREATE_FOOD_TABLE =
+                "CREATE TABLE " + iCareContract.FoodEntry.TABLE_NAME + "(" +
+                        iCareContract.FoodEntry._ID + "INTEGER PRIMARY KEY," +
+                        iCareContract.FoodEntry.COLUMN_FOOD_ITEM_NAME + "TEXT NOT NULL," +
+                        " )";
+
+        // SQL statement in order to create Exercise table
+        final String SQL_CREATE_EXERCISE_TABLE =
+                "CREATE TABLE " + iCareContract.ExerciseEntry.TABLE_NAME + "(" +
+                        iCareContract.ExerciseEntry._ID + "INTEGER PRIMARY KEY," +
+                        iCareContract.ExerciseEntry.COLUMN_EXERCISE_NAME + "TEXT NOT NULL," +
+                " )";
+
+        // Execute sql statement in order to create sql table in database
+        db.execSQL(SQL_CREATE_EXERCISE_TABLE);
+        db.execSQL(SQL_CREATE_FOOD_TABLE);
     }
 
     @Override
     public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
 
+        // SQL statement in order to delete table in the database
+        final String SQL_DELETE_FOOD_TABLE =
+                "DROP TABLE IF EXISTS " + iCareContract.FoodEntry.TABLE_NAME ;
+        final String SQL_DELETE_EXERCISE_TABLE =
+                "DROP TABLE IF EXISTS " + iCareContract.ExerciseEntry.TABLE_NAME;
+
+        db.execSQL(SQL_DELETE_EXERCISE_TABLE);
+        db.execSQL(SQL_DELETE_FOOD_TABLE);
+        onCreate(db);
+    }
+
+    public void onDowngrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+        onUpgrade(db, oldVersion, newVersion);
     }
 }
